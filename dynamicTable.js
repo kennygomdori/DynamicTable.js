baafac7ef1e889787354b24eec8760f1060427c2
kennygomdori <kennygomdori@gmail.com>
2019-12-13 18:29:32 -0800

    Complete overhaul
    Made it an ES6 class, and added the "append" method.
    Also has a test function.

--- dynamicTable.js
+++ dynamicTable.js
@@ -1,76 +1,157 @@
 /*
 dynamicTable.js
-copyright 2015, Keun Hoi Kim 
+copyright 2015, Keun Hoi Kim
 */
 
 
-function dynamicTable(_id, _class){
-	this._id = _id;
-	this._class = _class;
-	let table = document.getElementById(this._id).appendChild(document.createElement('table'));
-	table.className = this._class + " dynamicTable";
+export class DynamicTable {
+    constructor(container, data, header) {
+        this.table = document.createElement('table');
+        this.table.classList.add('dynamic-table');
+        if (Array.isArray(data)) this.tabulate(data, header);
+        container.appendChild(this.table);
+    }
 
-	this.tabulate = function(data, header) {
-		// format is either 'object' or 'array', or no data is drawn.
-		/*
-			array: [[value1, value2, value3],[value1,value2,value3], ...]
-			object: [{key1:v1, key2:v2, key3:v3}, ...]
-		*/
-		// header is either an array or true. Anything else will result in no header.
-		/*
-			header: [th1, th2, th3 ...]
-			If header === true,  the first row (if format === "array"), or the keys of the first row (if format === "object")
-		*/
-		
-		clear();
+    tabulate(data, header = true, append = false) {
+        /*
+        "format" is either 'object' or 'array', or no data is drawn.
+            array: [[row1_value1, row1_value2, row1_value3], [row2_value1, row2_value2, row2_value3], ...]
+            object: [{column1_name: v1, column2_name: v2, column3_name: v3}, ...]
+        The second format requires header in an array.
 
-		let format = "";
-		// data type determination
-		if (typeof data[0] === 'object') format = "object";
-		if (Array.isArray(data[0])) format = "array";
-		
-		// thead generation
-		if (format !== "object" && format !== "array"){
-			console.warn("The data is empty or in an invalid format."); 
-			return false;
-		}
-		
-		if (header === true){
-			let thead = table.appendChild(document.createElement('thead'));
-			let row = thead.appendChild(document.createElement('tr'));
-			if (format === "array"){
-				data[0].forEach(function(value){
-					row.appendChild(document.createElement('th')).appendChild(document.createTextNode(value));
-				});
-				data.shift();
-			}
-			if (format === "object"){
-				data[0].entries().forEach(function([key,value]){
-					row.appendChild(document.createElement('th')).appendChild(document.createTextNode(key));
-				});
-			}
-		}
-		if (typeof header === "array") {
-			let thead = table.appendChild(document.createElement('thead'));
-			let row = thead.appendChild(document.createElement('tr'));
-			header.forEach(function(value){
-				row.appendChild(document.createElement('th')).appendChild(document.createTextNode(value));
-			});
-		}
+        "header" is either an array or true. Anything else will result in no header.
+            header: [th1, th2, th3 ...]
+            If header === true and format === "array",  the first row will be used as the header.
+            data in object format requires its header in an array form otherwise, no header.
+        */
 
-		// tbody generation
-		let tbody = table.appendChild(document.createElement('tbody'));
-		data.forEach(function(array){
-			let row = tbody.appendChild(document.createElement('tr'));
-			array.forEach(function(value){
-				row.appendChild(document.createElement('td')).appendChild(document.createTextNode(value));
-			});
-		});
-	};
+        if (!append) this.clear();
+        const table = this.table;
 
-	const clear = this.clear = () => {
-		while (table.firstChild) {
-    		table.removeChild(table.firstChild);
-		}
-	};
+        let format = "";
+        // data type determination
+        if (Array.isArray(data[0])) format = "array";
+        else if (data[0] instanceof Object) {
+            if (!Array.isArray(header)) throw "A dataset with object datums requires an array header.";
+            format = "object";
+        } else throw "The data is empty or in an invalid format. 'data' needs to be an Object or an Array.";
+        let i = 0;
+
+        // thead generation
+        if (append) {
+            // don't add the header when appending.
+        } else if (header === true) {
+            const thead = table.appendChild(document.createElement('thead'));
+            const header_row = thead.appendChild(document.createElement('tr'));
+            if (format === "array") {
+                data[0].forEach(function (value) {
+                    header_row.appendChild(document.createElement('th')).appendChild(document.createTextNode(value));
+                });
+                i = 1;
+            } else if (format === "object") {
+                console.warn("header needs to be an array if the datum format is 'object'.")
+            }
+        } else if (Array.isArray(header)) {
+            const thead = table.appendChild(document.createElement('thead'));
+            const header_row = thead.appendChild(document.createElement('tr'));
+            header.forEach(function (value) {
+                header_row.appendChild(document.createElement('th')).appendChild(document.createTextNode(value));
+            });
+        }
+
+        // tbody generation
+        let tbody;
+        if (append) tbody = table.querySelector('tbody');
+        else tbody = table.appendChild(document.createElement('tbody'));
+        if (format === "array") {
+            for (; i < data.length; i++) {
+                const row = tbody.appendChild(document.createElement('tr'));
+                const row_data = data[i]
+                for (let j = 0; j < row_data.length; j++) {
+                    row.appendChild(document.createElement('td')).appendChild(document.createTextNode(row_data[j]));
+                }
+            }
+        } else if (format === "object") {
+            for (; i < data.length; i++) {
+                const row = tbody.appendChild(document.createElement('tr'));
+                const row_data = data[i];
+                for (let j = 0; j < header.length; j++) {
+                    row.appendChild(document.createElement('td')).appendChild(document.createTextNode(row_data[header[j]]));
+                }
+            }
+        }
+    };
+
+    clear() {
+        this.table.innerHTML = "";
+    }
+
+    append(data, header = true) {
+        this.tabulate(data, header, true)
+    }
+}
+
+const test = () => {
+    let table1 = new DynamicTable(document.body,
+        [
+            {name: "Locke", number: 4},
+            {name: "Reyes", number: 8},
+            {name: "Ford", number: 15},
+            {name: "Jarrah", number: 16},
+            {name: "Shephard", number: 23},
+            {name: "Kwon", number: 42}
+        ], ['name', 'number']
+    );
+
+    let table2 = new DynamicTable(document.body,
+        [
+            [11975, 5871, 8916, 2868],
+            [1951, 10048, 2060, 6171],
+            [8010, 16145, 8090, 8045],
+            [1013, 990, 940, 6907]
+        ], true
+    );
+
+    let table3 = new DynamicTable(document.body,
+        [
+            [11975, 5871, 8916, 2868],
+            [1951, 10048, 2060, 6171],
+            [8010, 16145, 8090, 8045],
+            [1013, 990, 940, 6907]
+        ], ['num1', 'num2', 'num3', 'num4']
+    );
+
+    try {
+        let table4 = new DynamicTable(document.body,
+            [
+                {name: "Locke", number: 4},
+                {name: "Reyes", number: 8},
+                {name: "Ford", number: 15},
+                {name: "Jarrah", number: 16},
+                {name: "Shephard", number: 23},
+                {name: "Kwon", number: 42}
+            ], true
+        );
+    } catch (e) {
+        // should throw here.
+        console.warn(e);
+    }
+
+    table1.append([
+        [11975, 5871, 8916, 2868],
+        [1951, 10048, 2060, 6171],
+        [8010, 16145, 8090, 8045],
+        [1013, 990, 940, 6907]
+    ]);
+
+    table2.append([
+            {name: "Locke", number: 4},
+            {name: "Reyes", number: 8},
+            {name: "Ford", number: 15},
+            {name: "Jarrah", number: 16},
+            {name: "Shephard", number: 23},
+            {name: "Kwon", number: 42}
+        ], ['name', 'number']);
+
+    console.log("CLEAR.");
 }
